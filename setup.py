901cf1a83c7d30cafd8a907e8790ac17ec86eb61
tbbatbb <20682299+tbbatbb@users.noreply.github.com>
2024-05-20 21:38:40 +0800

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Please refer to the homepage of the library
 
 setuptools.setup(
   name="wwbot",
-  version="0.0.8a",
+  version="0.0.8b",
   author="tbbatbb",
   author_email="20682299+tbbatbb@users.noreply.github.com",
   description="A library for dealing with messages in WeWork",
