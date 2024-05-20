acdf9807cacba19b89cbe4b2ecc05f9b2f032fb0
tbbatbb <20682299+tbbatbb@users.noreply.github.com>
2024-05-20 10:19:59 +0800

    update the long description

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 
 import setuptools
 
+long_description = '''
+Please refer to the homepage of the library
+'''
+
 setuptools.setup(
   name="wwbot",
   version="0.0.6a",
@@ -8,6 +12,8 @@ setuptools.setup(
   author_email="20682299+tbbatbb@users.noreply.github.com",
   description="A library for dealing with messages in WeWork",
   url="https://github.com/tbbatbb/WeWorkBot",
+  long_description_content_type='text/markdown',
+  long_description=long_description,
   packages=setuptools.find_packages(),
   classifiers=[
     "Development Status :: 2 - Pre-Alpha",
