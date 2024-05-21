3c457b99faf5c55c75b1ea99491ac0538330121d
tbbatbb <20682299+tbbatbb@users.noreply.github.com>
2024-05-21 22:23:33 +0800

    example: send message

---
+++ examples/send_msg.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8
+
+from flask import Flask
+from wwbot import WWBot
+from wwbot.msg import TextMessage
+
+app:Flask = Flask('EchoBot')
+
+corp_id:str = 'ww274398273492874'
+corp_secret:str = '23kjs9dufij3234WERq234rwer234'
+
+# agent id is required when sending a message
+WWBot.send_to(corp_id, corp_secret, TextMessage('tbbatbb', corp_id, 'hello from tbbatbb!', agent_id='1000007'))
+
