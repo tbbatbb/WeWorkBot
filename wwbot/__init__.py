9a2c893285db3987ac7641a601a50b0a1e80f355
tbbatbb <20682299+tbbatbb@users.noreply.github.com>
2024-05-22 10:15:41 +0800

    activate the decorator

--- wwbot/__init__.py
+++ wwbot/__init__.py
@@ -262,3 +262,7 @@ def location_default(msg:LocationMessage) -> Message:
 @WWBot.on('link')
 def link_default(msg:LinkMessage) -> Message:
     return TextMessage(msg.from_username, msg.to_username, msg.agent_id, f'{msg.title}\n{msg.description}\n{msg.url}\n{msg.pic_url}')
+
+@WWBot.verify_handler()
+@WWBot.request_handler()
+def useless(): pass 
