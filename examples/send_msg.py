680434fca6272b2417a50c79c6efa8cc3ec4c501
tbbatbb <20682299+tbbatbb@users.noreply.github.com>
2024-05-22 09:36:23 +0800

    update the example code for 'send_to' with TextCardMessage

--- examples/send_msg.py
+++ examples/send_msg.py
@@ -2,14 +2,27 @@
 
 from flask import Flask
 from wwbot import WWBot
-from wwbot.msg import TextMessage
+from wwbot.msg import TextCardMessage
 
 app:Flask = Flask('EchoBot')
 
 corp_id:str = 'ww274398273492874'
 corp_secret:str = '23kjs9dufij3234WERq234rwer234'
+to_username:str = 'tbbatbb'
 agent_id = '1000004'
 
+txtcard_msg = TextCardMessage(
+    to_username, 
+    corp_id, 
+    agent_id, 
+    'TextCard消息测试', 
+    f'一条给 $userName={to_username}$ 的测试消息', 
+    'https://www.qq.com', 
+    'More', 
+    safe=True,
+    enable_id_trans=True
+)
+
 # agent id is required when sending a message
-WWBot.send_to(corp_id, corp_secret, TextMessage('tbbatbb', corp_id, agent_id, 'hello from tbbatbb!'))
+WWBot.send_to(corp_id, corp_secret, txtcard_msg)
 
