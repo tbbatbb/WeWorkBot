bd5b92d60b2c7bc1ba6d25f81cb064326d3f0542
tbbatbb <20682299+tbbatbb@users.noreply.github.com>
2024-05-27 08:55:28 +0800

    update example

--- examples/send_msg.py
+++ examples/send_msg.py
@@ -1,12 +1,13 @@
 # -*- encoding: utf-8
 
 from wwbot import WWBot
-from wwbot.msg import TextCardMessage
+from wwbot.msg import TextMessage, TextCardMessage
 
 corp_id:str = 'ww274398273492874'
 corp_secret:str = '23kjs9dufij3234WERq234rwer234'
 to_username:str = 'tbbatbb'
 agent_id = '1000004'
+group_bot_key:str = '6449f223-4920-2394-3aa39d939ae0'
 
 txtcard_msg = TextCardMessage(
     to_username, 
@@ -24,4 +25,9 @@ txtcard_msg = TextCardMessage(
 
 # agent id is required when sending a message
 WWBot.send_to(corp_id, corp_secret, txtcard_msg)
+
+
+# sending message as group bot
+text_msg = TextMessage(None, None, None, 'a text message sent by group chat bot', for_group_bot=True, group_bot_key=group_bot_key)
+WWBot.send_to(corp_id, corp_secret, text_msg)
 
